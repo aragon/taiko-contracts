dda1f68d27a7687de4819daced170df13160aad4
Daniel Wang <dan@taiko.xyz>
2024-10-01 23:03:53 +0800

    minor comment change

--- src/adapted-dependencies/TaikoL1.sol
+++ src/adapted-dependencies/TaikoL1.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.17;
 
 /// @dev Manual copy of lib/taiko-mono/packages/protocol/contracts/L1/TaikoL1.sol and its dependent structs
+/// from https://github.com/taikoxyz/taiko-mono/tree/protocol-v1.9.0.
 /// This is in order to overcome the conflicting compiler versions between 0.8.17 (OSx) and 0.8.24 (Taiko)
 abstract contract TaikoL1 {
     /// @notice Returns true if the contract is paused, and false otherwise.
